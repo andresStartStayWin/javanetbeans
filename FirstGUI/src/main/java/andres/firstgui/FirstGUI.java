328d2b6822c9c0fadc122d6d468566c78bd89cae
andresStartStayWin <andrescarabajal@gmail.com>
2023-04-13 22:58:33 -0300

    java

---
+++ FirstGUI/src/main/java/andres/firstgui/FirstGUI.java
@@ -0,0 +1,26 @@
+
+package andres.firstgui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class FirstGUI {
+    
+    public GUI {
+        JFrame frame = new JFrame();
+        
+        
+        JPanel panel = new JPanel();
+        panel.setBorder(border);
+}
+
+                
+   
+    
+public static void main(String[] args) {
+    new GUI();
+    
+    }
+}
