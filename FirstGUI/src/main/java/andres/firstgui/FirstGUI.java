d5f7c4c0136498b5e66148e469f216364002afcd
andresStartStayWin <andrescarabajal@gmail.com>
2023-04-15 18:55:39 -0300

    subo la pantalla JFrame

--- FirstGUI/src/main/java/andres/firstgui/FirstGUI.java
+++ FirstGUI/src/main/java/andres/firstgui/FirstGUI.java
@@ -1,26 +1,9 @@
-
 package andres.firstgui;
-
-import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 
 public class FirstGUI {
     
-    public GUI {
-        JFrame frame = new JFrame();
-        
-        
-        JPanel panel = new JPanel();
-        panel.setBorder(border);
-}
-
-                
-   
-    
-public static void main(String[] args) {
-    new GUI();
-    
+    public static void main(String[] args) {
+        pantalla x = new pantalla();
+        x.setVisible(true);
     }
 }
