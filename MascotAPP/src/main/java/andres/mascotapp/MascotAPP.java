bdaebbebdf9c045a852dc0646becc92714e4637f
AndresPC <andrescarabajal@gmail.com>
2023-08-07 16:32:39 -0300

    java

--- MascotAPP/src/main/java/andres/mascotapp/MascotAPP.java
+++ MascotAPP/src/main/java/andres/mascotapp/MascotAPP.java
@@ -3,6 +3,8 @@
  */
 package andres.mascotapp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author andre
@@ -19,9 +21,33 @@ package andres.mascotapp;
 public class MascotAPP {
 
     public static void main(String[] args) {
-
+/*
        String[] nombresArray = new String[5];
-    }
+       
+        for (int i = 0; i < nombresArray.length; i++) {
+            nombresArray[i] = "Chiquito "+(i+1);
+            
+        }
+        
+        for (String var : nombresArray) {
+            System.out.println(var);
+            
+        }
+*/
+
+    ArrayList<String> nombresArrayList = new ArrayList();
+
+    nombresArrayList.add("Chiquito");
+    nombresArrayList.add("Chiquito");
+    nombresArrayList.add("Chiquito");
+    
+        System.out.println(nombresArrayList.size());
+        
+        
+        nombresArrayList.remove("Chiquito");
+        System.out.println(nombresArrayList.size());
+        
+}
     
     
 }
--- MascotAPP/src/main/java/andres/mascotapp/Servicios/ServicioMascotas.java
+++ MascotAPP/src/main/java/andres/mascotapp/Servicios/ServicioMascotas.java
@@ -4,10 +4,47 @@
  */
 package andres.mascotapp.Servicios;
 
+import andres.mascotapp.entidades.Mascota;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author andre
  */
 public class ServicioMascotas {
+    private Scanner leer = new Scanner(System.in).useDelimiter("\n");
     
+    private List<String> mascotas;
+     
+    public ServicioMascotas() {
+        this.mascotas = new ArrayList();
+    }
+            
+    public void crearMascota() {
+        System.out.println("nombre");
+        String nombre = leer.next();
+        
+        System.out.println("apodo");
+        String apodo = leer.next();
+        
+        System.out.println("tipo");
+        String tipo = leer.next();
+        
+        String mascota = nombre+" "+apodo+" "+tipo;
+        mascotas.add(mascota);
+    }
+            
+    
+    public void mostrarMascotas(){
+        System.out.println("Las mascotas son ");
+        for (String aux : mascotas) {
+            
+            System.out.println(aux);
+            
+        }
+        System.out.println("cantidad = "+mascotas.size());
+    }
+            
 }
