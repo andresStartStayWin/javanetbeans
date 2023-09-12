53554dd891d3d7b364292ee4088709324b7ddbc3
AndresPC <andrescarabajal@gmail.com>
2023-09-11 21:55:43 -0300

    java

--- HerenciaEj2Electrodom/src/Entidades/Elec1.java
+++ HerenciaEj2Electrodom/src/Entidades/Elec1.java
@@ -8,17 +8,15 @@ package Entidades;
  *
  * @author andre
  */
-
-
-public class Electrodomestico {
-
+public class Elec1 {
+    
     protected double precio, peso;
     protected String color, consumoEnergetico;
 
-    public Electrodomestico() {
+    public Elec1() {
     }
 
-    public Electrodomestico(double precio, double peso, String color, String consumoEnergetico) {
+    public Elec1(double precio, double peso, String color, String consumoEnergetico) {
         this.precio = precio;
         this.peso = peso;
         this.color = color;
@@ -56,5 +54,15 @@ public class Electrodomestico {
     public void setConsumoEnergetico(String consumoEnergetico) {
         this.consumoEnergetico = consumoEnergetico;
     }
-
+    
+    private void comprobarConsumo(String letra) {
+        if (letra >= "A" && letra <= "F") {
+            consumoEnergetico = letra;
+        }
+        else {
+            consumoEnergetico = "F";
+        }
+    }
+    
+    
 }
